22a3e7ba44eeaad1a2859818d14ad8ac670ef643
Prakhar Ojha <prakharojha12@gmail.com>
2022-12-26 11:22:01 +0530

    React router added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,53 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from "./pages/Error";
+import Home from './pages/Home'
+import { Provider } from 'react-redux'  
+import {store} from './configureStore'
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+  backgroundColors:{
+    1:"#82AAE3" 
+  }
+}
+
+const theme = extendTheme({ colors })
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children:[
+      {
+        path:"home",
+        element:<Home />
+      },
+    ],
+  },
+]);
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+    </Provider>
   </React.StrictMode>,
 )
